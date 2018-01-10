86c05241a975ac0effdf7c22fc2a86c66fc771ba
Henrik Christensen <sensen1695@hotmail.com>
2018-01-10 22:36:15 +0100

    guessing_games models created

--- kemispil/guessing_games/models.py
+++ kemispil/guessing_games/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class GuessingGame(models.Model):
+    game_title = models.CharField(max_length=50)
+    game_description = models.TextField()
+
+    class Meta:
+        ordering = ['game_title']
+
+    def __str__(self):
+        return 'Game: {}'.format(self.game_title)
+
+
+class GameItem(models.Model):
+    item_key = models.CharField(max_length=50)
+    item_value = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return '{}: {}'.format(self.item_key, self.item_value)
