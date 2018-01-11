c3fab5614cded8895fcbc5ddd69e19f33ebb6d6e
Henrik Christensen <sensen1695@hotmail.com>
2018-01-11 19:30:54 +0100

    guessing_games levels view + template created

--- kemispil/guessing_games/urls.py
+++ kemispil/guessing_games/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'guessing_games'
 urlpatterns = [
+    path('vendespil/<topic>/levels/', views.display_levels, name='levels'),
     path('vendespil/', views.GuessingGamesListView.as_view(), name='guessing_games_list'),
 ]
--- kemispil/guessing_games/views.py
+++ kemispil/guessing_games/views.py
@@ -4,6 +4,14 @@ from django.shortcuts import render
 from . import models
 
 
+def display_levels(request, topic):
+    title = topic
+    return render(request, 'guessing_games/guessing_games_levels.html', {
+        'levels': range(1, 5),
+        'title': title
+    })
+
+
 class GuessingGamesListView(generic.ListView):
     model = models.GuessingGame
     template_name = 'guessing_games/guessing_games_list.html'
