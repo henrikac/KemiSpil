0d54cab1807395229c9b451b95aeb2551faa6fc4
Henrik Christensen <sensen1695@hotmail.com>
2018-01-14 11:51:27 +0100

    added env var getter

--- kemispil/kemispil/settings.py
+++ kemispil/kemispil/settings.py
@@ -11,6 +11,21 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import warnings
+
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = 'Set the {} env variable'.format(var_name)
+        if DEBUG:
+            warnings.warn(error_msg)
+        else:
+            raise ImproperlyConfigured(error_msg)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
